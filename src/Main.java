7a17c2ee16bfc971846349b41c540b6aa0e4f3a1
n-ferrari <nathi.ferrari@hotmail.com>
2023-09-29 19:14:10 -0300

    question 1.1

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+//Is Unique: Implement an algorithm to determine if a string has all unique characters.
+// What if you cannot use additional data structures?
+ //       Hints: #44, #117, #132
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isUnique("wsehua"));
+    }
+    public static boolean isUnique(String word){
+        char[] lettersWord = new char[100];
+
+        for (int i = 0; i < word.length(); i++) {
+            for (int j = 0; j < lettersWord.length; j++) {
+                if(lettersWord[j] == '\u0000'){
+                    lettersWord[j] = word.charAt(i);
+                    break;
+                }else{
+                    if(lettersWord[j] == word.charAt(i)){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
