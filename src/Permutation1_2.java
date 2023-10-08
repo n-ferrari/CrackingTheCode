3aeb2bdbe2f7aca1640da2d09e7a1906dd8c4d34
n-ferrari <nathi.ferrari@hotmail.com>
2023-10-08 14:23:24 -0300

    list 1

--- src/Permutation1_2.java
+++ src/Permutation1_2.java
@@ -1,7 +1,7 @@
 //Check Permutation: Given two strings, write a method to decide if one is a permutation of the other.
 public class Permutation1_2 {
     public static void main(String[] args) {
-        String one = "Abc";
+        String one = "cbA";
         String two = "Abc";
         System.out.println(isPermutation(one, two));
     }
